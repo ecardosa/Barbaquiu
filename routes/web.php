1f946979d7988f9f96f04843c90eb50b210bc3df
ecardosa <ecardosa@cendrassos.net>
2024-04-19 16:27:12 +0200

    chat page

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,12 @@ use Inertia\Inertia;
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BarbecuesController;
+use App\Http\Controllers\TestController;
 
 Route::get('/', [IndexController::class, 'show'])->name('index');
+
+Route::get('/test', [TestController::class, 'index'])->name('test');
 
 Route::middleware('auth')->group(function () {
     Route::resource('profile', ProfileController::class)->only(['edit', 'update', 'destroy']);
